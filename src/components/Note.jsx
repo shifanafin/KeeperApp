afa259fb10d08286e878963e66caefedaa51aa3b
shifanafin <ctshifanachennara@gmail.com>
2022-07-09 16:06:50 +0530

    try to add style but not added

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -11,7 +11,9 @@ function handleDelete()
     <div className='note'>
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={handleDelete}>DELETE</button>
+        <button onClick={handleDelete}>
+       Delete
+        </button>
     </div>
   )
 }
